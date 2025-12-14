8398df0721eebf92528b07c40f1ccc4ced3fc7f0
GoatMudryk <alfioke7@gmail.com>
2025-12-15 01:45:24 +0700

    Feat: Membuat fitur Pencapaian

--- lib/models/pencapaian_model.dart
+++ lib/models/pencapaian_model.dart
@@ -3,8 +3,8 @@ class Pencapaian {
   final String nama;
   final String? deskripsi;
   final String? thumbnail;
-  
   final String? tanggalDidapat;
+  final bool isUnlocked; 
 
   Pencapaian({
     required this.id,
@@ -12,6 +12,7 @@ class Pencapaian {
     this.deskripsi,
     this.thumbnail,
     this.tanggalDidapat,
+    this.isUnlocked = false, 
   });
 
   factory Pencapaian.fromJson(Map<String, dynamic> json) {
@@ -23,6 +24,7 @@ class Pencapaian {
       tanggalDidapat: json['pivot'] != null 
           ? json['pivot']['created_at'] 
           : null,
+      isUnlocked: json['is_unlocked'] ?? false,
     );
   }
 }
